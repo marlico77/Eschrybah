512fceeafb76a6f127280c0c06624d7ba1acab9a
marlico77 <marlonssoficial@gmail.com>
2026-09-14 17:55:50 -0300

    Corrigindo nome para Eschrybah e alterando bundleId para com.core

--- Sources/LibraryViewController.swift
+++ Sources/LibraryViewController.swift
@@ -23,7 +23,7 @@ class LibraryViewController: UIViewController, UIDocumentPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Escribah"
+        title = "Eschrybah"
         // Cor creme clássica para fundo de leitura
         view.backgroundColor = UIColor(red: 249/255, green: 246/255, blue: 240/255, alpha: 1.0)
         
